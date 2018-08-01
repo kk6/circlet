802ac92617662a996ee76964f8b8124b4cc8dfd7
kk6 <hiro.ashiya@gmail.com>
2018-08-01 10:15:01 +0900

    Replace login view to CBV

--- circlet/users/urls.py
+++ circlet/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login, name="login"),
+    path("login/", views.TwitterLoginRedirectView.as_view(), name="login"),
     path("callback/", views.callback, name="callback"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path(
--- circlet/users/views.py
+++ circlet/users/views.py
@@ -13,16 +13,20 @@ from .api import create_or_update_twitter_account
 from .models import TwitterAccount, UserSettings, Friendship
 
 
-def login(request):
-    auth = tweepy.OAuthHandler(
-        settings.CONSUMER_KEY, settings.CONSUMER_SECRET, settings.CALLBACK_URL
-    )
-    try:
-        redirect_url = auth.get_authorization_url()
-    except tweepy.TweepError:
-        raise tweepy.TweepError("Error! Failed to get request token")
-    request.session["request_token"] = auth.request_token
-    return redirect(redirect_url)
+class TwitterLoginRedirectView(RedirectView):
+    oauth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET, settings.CALLBACK_URL)
+
+    def get_authorization_url(self):
+        try:
+            url = self.oauth.get_authorization_url()
+        except tweepy.TweepError:
+            raise tweepy.TweepError("Error! Failed to get request token")
+        return url
+
+    def get_redirect_url(self, *args, **kwargs):
+        self.url = self.get_authorization_url()
+        self.request.session["request_token"] = self.oauth.request_token
+        return super().get_redirect_url(*args, **kwargs)
 
 
 def callback(request):
